3dc74a8336486ef4fb9293a66aa35d6553617ee3
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-07-05 14:16:18 -0400

    Initial Commit

---
+++ TCAAppFinal/TCAAppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAppFinalApp.swift
+//  TCAAppFinal
+//
+//  Created by Matthew Garlington on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TCAAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
